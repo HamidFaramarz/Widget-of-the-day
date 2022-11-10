b844323e04aa95fb40f18be3dd840317ce336b49
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2022-11-10 08:58:21 +0430

    Update main.dart

--- FlutterLogo/main.dart
+++ FlutterLogo/main.dart
@@ -13,3 +13,16 @@ Container(
                     duration: Duration(seconds: 5),
                     size: 150,
                   )))
+
+  
+  // Second Example
+  
+  child: Align(
+                  alignment: Alignment(0.9, 1),
+                  child: FlutterLogo(
+                    size: 150,
+                    curve: Curves.bounceInOut,
+                    style: FlutterLogoStyle.horizontal,
+                    duration: Duration(seconds: 3),
+                    textColor: Colors.red,
+                  ))
