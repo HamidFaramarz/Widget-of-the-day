b2989b8c4d8ffb2d02a7e480be6ba0b01cda57f9
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2022-11-10 09:31:36 +0430

    Create main.dart

---
+++ Baseline/main.dart
@@ -0,0 +1,10 @@
+Container(
+            color: Colors.blue,
+            //alignment: Alignment.center,
+            width: double.infinity,
+            height: 150.0,
+            child: Baseline(
+              baseline: 100,
+              baselineType: TextBaseline.ideographic,
+              child: Text("Name"),
+            ))
