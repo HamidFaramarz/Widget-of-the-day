d9883578c41c37b28dcfedfa03b55776ad53d07f
Hamid Faramarz <101446076+HamidFaramarz@users.noreply.github.com>
2022-11-10 08:54:53 +0430

    Create main.dart

---
+++ FlutterLogo/main.dart
@@ -0,0 +1,15 @@
+Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
+              //alignment: Alignment.centerLeft,
+              width: 200,
+              height: 200,
+              color: Colors.amber,
+              child: Align(
+                  alignment: Alignment(0.9, 1),
+                  child: FlutterLogo(
+                    style: FlutterLogoStyle.stacked,
+                    curve: Curves.easeInBack,
+                    duration: Duration(seconds: 5),
+                    size: 150,
+                  )))
